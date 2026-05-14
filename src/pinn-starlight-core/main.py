77b503a6ad846f372169f71856ec6e161bd48a98
sorrime <niangaoa@hotmial.com>
2026-05-14 21:42:45 +0800

    wip: Study MNIST

--- src/pinn-starlight-core/main.py
+++ src/pinn-starlight-core/main.py
@@ -1,21 +1,11 @@
 # This is a sample Python script.
 import numpy
 
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-a = [[3,2],[4,5]]
-
-a[numpy.arange(10) , 1] = 1
-
+a = numpy.array([3,2])
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print(a)
+    a[numpy.arange(0), 0] = 1
     print(a)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
