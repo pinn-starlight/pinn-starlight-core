215ebc54e87136e77507bcf1ffb70496a668f185
sorrime <niangaoa@hotmial.com>
2026-07-08 11:35:06 +0800

    dev: 测试出图

--- scripts/PhotoTraining.py
+++ scripts/PhotoTraining.py
@@ -54,8 +54,8 @@ for file in sorted(os.listdir(input_dir)):
         lr = 0.001
     )
 
-    for step in tqdm(range(int(coords.shape[0] * 0.000618))):
-        idx = torch.randint(0, coords.shape[0], (int(coords.shape[0]  * 0.00618 * 0.7),))
+    for step in tqdm(range(int(coords.shape[0] * 0.00618 * 0.4))):
+        idx = torch.randint(0, coords.shape[0], (int(coords.shape[0]  * 0.00618 * 0.3),))
         batch_xy = coords[idx].to(device).clone().requires_grad_(True)
         batch_I = values[idx].to(device)
 
